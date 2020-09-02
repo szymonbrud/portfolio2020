34017412b41aa20bdedf30077c76842beeffedc6
szymonqqaz <szymonqqaz22@gmail.com>
2020-09-02 22:21:16 +0200

    Add absolute import

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
@@ -6,9 +8,18 @@ module.exports = {
     author: '@gatsbyjs',
   },
   plugins: [
-    'gatsby-plugin-eslint',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages'),
+        components: path.join(__dirname, 'src/components'),
+        assets: path.join(__dirname, 'src/assets'),
+        images: path.join(__dirname, 'src/images'),
+      },
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
