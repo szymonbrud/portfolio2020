dc60546cd7f38d90bc65d9d53571b8d774bbe980
szymonqqaz <szymonqqaz22@gmail.com>
2020-09-04 15:03:04 +0200

    Change two file names

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 import './styles.css';
 
 import GlobalStyleProvider from 'assets/styles/globalStyle';
-import TopBarPhone from 'components/TopBarPhone';
+import TopBarPhone from 'components/TopBarMobile';
 import Background from 'components/Background';
-import HeroTextPhone from 'components/HeroTextPhone';
+import HeroTextPhone from 'components/HeroText';
 import NavigationArrow from 'components/NavigationArrow';
 import ScrollDownLineHero from 'components/ScrollDownLineHero';
 import PageNumber from 'components/PageNumber';
