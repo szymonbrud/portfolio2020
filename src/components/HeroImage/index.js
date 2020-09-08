484404747be8e95bfab1a1b3248c7e8372a12b0f
szymonqqaz <szymonqqaz22@gmail.com>
2020-09-08 12:36:48 +0200

    Change way to load my image, to gatsby-images

--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { graphql, StaticQuery } from 'gatsby';
-// import BackgroundImage from 'gatsby-background-image';
+import Img from 'gatsby-image';
+
+import styled from 'styled-components';
+import media from 'assets/styles/media';
 
 import MyPhoto from 'assets/images/me_black.jpg';
 
-import { Image, Background, Wrapper } from './style';
+import { Background, Wrapper } from './style';
 import useAnimationHook from './animationHook';
+
+const Image = styled(Img)`
+  position: relative;
+  z-index: 3;
+  height: 46vh;
+  width: calc(46vh * 0.75);
+
+  ${media.smallDesktop`
+    height: 60vh;
+    width: calc(60vh * 0.75);
+  `}
+`;
 
 const HeroImage = () => {
   const { textWrapper } = useAnimationHook();
@@ -14,7 +29,7 @@ const HeroImage = () => {
     <StaticQuery
       query={graphql`
         query {
-          images: file(relativePath: { eq: "background.png" }) {
+          images: file(relativePath: { eq: "me_black.jpg" }) {
             childImageSharp {
               fluid(quality: 90, maxWidth: 1920) {
                 ...GatsbyImageSharpFluid_withWebp
@@ -23,14 +38,16 @@ const HeroImage = () => {
           }
         }
       `}
-      render={() => (
-        // const imageData = data.images.childImageSharp.fluid;
+      render={data => {
+        const imageData = data.images.childImageSharp.fluid;
 
-        <Wrapper ref={textWrapper}>
-          <Image src={MyPhoto} />
-          <Background />
-        </Wrapper>
-      )}
+        return (
+          <Wrapper ref={textWrapper}>
+            <Image src={MyPhoto} fluid={imageData} />
+            <Background />
+          </Wrapper>
+        );
+      }}
     />
   );
 };
