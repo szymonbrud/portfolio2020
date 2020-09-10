3f97b7917f92b4db413aed18efcec32634f7293b
szymonqqaz <szymonqqaz22@gmail.com>
2020-09-10 11:43:57 +0200

    I created a E2E tests

--- cypress/integration/Index/HeroPage.js
+++ cypress/integration/Index/HeroPage.js
@@ -1,6 +1,25 @@
 describe('Testing HeroPage', () => {
-  it('Join to HeroPage', () => {
+  beforeEach(() => {
+    cy.viewport(1920, 1080);
     cy.visit('http://localhost:8000');
+    cy.wait(5000);
+  });
+  it('Join to HeroPage', () => {
     cy.contains('Szymon Brud');
+  });
+
+  it('Go to projects page by scroll down', () => {
+    cy.contains('scroll down').click();
+    cy.url().should('include', '/projects');
+  });
+
+  it('Go to projects by click arrow down', () => {
+    cy.get('.1').click();
+    cy.url().should('include', '/projects');
+  });
+
+  it('Go to contact by click arrow up', () => {
+    cy.get('.0').click();
+    cy.url().should('include', '/contact');
   });
 });
